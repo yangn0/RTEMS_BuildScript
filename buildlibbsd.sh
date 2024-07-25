e67c48ab7240788561103e3f6ef92db30f5d1c76
Ning Yang <yangn0@qq.com>
2024-07-26 01:28:46 +0800

    change buildlibbsd.sh

--- buildlibbsd.sh
+++ buildlibbsd.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+export bsp=aarch64/raspberrypi4b
 #aarch64/raspberrypi4b
 #aarch64/xilinx_versal_qemu
 #xilinx_zynqmp_lp64_qemu
 #arm/xilinx_zynq_a9_qemu
-cd /home/yangn0/RTEMS_devel/src/rtems-libbsd
+cd ~/RTEMS_devel/src/rtems-libbsd
 # ./waf distclean
 ./waf configure --prefix=$HOME/RTEMS_devel/rtems/6 \
-      --rtems-bsps=arm/xilinx_zynq_a9_qemu \
+      --rtems-bsps=$bsp \
       --buildset=buildset/default.ini
 ./waf
 ./waf install
